110c3039ebe6e43510486359505a032d86fcc9db
Troy Martin <beef.erikson.studios@gmail.com>
2022-10-20 01:58:37 -0400

    first commit

---
+++ Chapter01/CodeAnalyzing/src/Program.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="Program.cs" company="Beef Erikson Studios">
+// Copyright (c) Beef Erikson Studios. All Rights Reserved.
+// </copyright>
+
+namespace CodeAnalyzing.src;
+
+using System.Diagnostics;
+
+/// <summary>
+/// The main class for this console app.
+/// </summary>
+public class Program
+{
+    /// <summary>
+    /// The main entry point for this console app.
+    /// </summary>
+    /// <param name="args">
+    /// A string array of arguments passed to the console app.
+    /// </param>
+    public static void Main(string[] args)
+    {
+        Debug.WriteLine("Hello, Debugger!");
+    }
+}
