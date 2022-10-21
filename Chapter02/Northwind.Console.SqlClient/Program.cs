a937d605fb25825e48b816f5129a2cc0c79db052
Troy Martin <beef.erikson.studios@gmail.com>
2022-10-21 00:35:02 -0400

    Chapter02 added. SQL server connection established.

--- Chapter02/Northwind.Console.SqlClient/Program.cs
+++ Chapter02/Northwind.Console.SqlClient/Program.cs
@@ -1,2 +1,118 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// <copyright file="Program.cs" company="Beef Erikson Studios">
+// Copyright (c) Beef Erikson Studios. All rights reserved.
+// </copyright>
+using Microsoft.Data.SqlClient;
+
+namespace Northwind.Console.SqlClient;
+
+/// <summary>
+/// The main class for the project.
+/// </summary>
+internal partial class Program
+{
+    /// <summary>
+    /// The main function of the project.
+    /// </summary>
+    /// <param name="args">command-line arguments.</param>
+    internal static void Main(string[] args)
+    {
+        SqlConnectionStringBuilder builder = new ();
+
+        builder.InitialCatalog = "Northwind";
+        builder.MultipleActiveResultSets = true;
+        builder.Encrypt = true;
+        builder.TrustServerCertificate = true;
+        builder.ConnectTimeout = 10;
+
+        WriteLine("Connect to:");
+        WriteLine("  1 - SQL Server on local machine");
+        WriteLine("  2 - Azure SQL Database");
+        WriteLine("  3 - Azure SQL Edge");
+        WriteLine();
+        Write("Press a key: ");
+
+        ConsoleKey key = ReadKey().Key;
+        WriteLine();
+        WriteLine();
+
+        if (key is ConsoleKey.D1 or ConsoleKey.NumPad1)
+        {
+            builder.DataSource = "."; // local SQL server
+        }
+        else if (key is ConsoleKey.D2 or ConsoleKey.NumPad2)
+        {
+            builder.DataSource = "tcp:apps-services-net7.database.windows.net,1433"; // Azure SQL Database
+        }
+        else if (key is ConsoleKey.D3 or ConsoleKey.NumPad3)
+        {
+            builder.DataSource = "tcp:120.0.0.1,1433"; // Azure SQL Edge
+        }
+        else
+        {
+            WriteLine("No data source selected.");
+            return;
+        }
+
+        WriteLine("Authenticate using:");
+        WriteLine("  1 - Windows Integrated Security");
+        WriteLine("  2 - SQL Login, for example, sa");
+        WriteLine();
+        Write("Press a key: ");
+
+        key = ReadKey().Key;
+        WriteLine();
+        WriteLine();
+
+        if (key is ConsoleKey.D1 or ConsoleKey.NumPad1)
+        {
+            builder.IntegratedSecurity = true;
+        }
+        else if (key is ConsoleKey.D2 or ConsoleKey.NumPad2)
+        {
+            builder.UserID = "sa"; // Azure SQL Edge - Change this to your username
+
+            Write("Enter your SQL Server password: ");
+            string? password = ReadLine();
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                WriteLine("Password cannot be empty or null.");
+                return;
+            }
+
+            builder.Password = password;
+            builder.PersistSecurityInfo = false;
+        }
+        else
+        {
+            WriteLine("No authentication selected.");
+            return;
+        }
+
+        SqlConnection connection = new (builder.ConnectionString);
+
+        WriteLine(connection.ConnectionString);
+        WriteLine();
+
+        connection.StateChange += Connection_StateChange;
+        connection.InfoMessage += Connection_InfoMessage;
+
+        try
+        {
+            WriteLine(
+                "Opening connection. Please wait up to {0} seconds...",
+                builder.ConnectTimeout);
+            WriteLine();
+            connection.Open();
+
+            WriteLine($"SQL Server version: {connection.ServerVersion}");
+
+            connection.StatisticsEnabled = true;
+        }
+        catch (SqlException ex)
+        {
+            WriteLine($"SQL exception: {ex.Message}");
+        }
+
+        connection.Close();
+    }
+}
